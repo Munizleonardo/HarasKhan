4e697679cae3bfe1d444d3e9f49068e74c61f7ab
Leonardo Muniz <113261741+Munizleonardo@users.noreply.github.com>
2025-10-18 19:40:28 -0300

    Finalização
    Finalização do site.

---
+++ Workspace/Javascript/global.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const currentPage = window.location.pathname.split('/').pop();
+    const navLinks = document.querySelectorAll('.nav-links a');
+
+    navLinks.forEach(link => {
+        const linkPage = link.getAttribute('href').split('/').pop();
+        
+        if (linkPage === currentPage) {
+            link.classList.add('active');
+        }
+    });
+});
